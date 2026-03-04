f11aecda24dd56f5b01ba78a56a66bf4d2103d36
Jana Schroth <schroth.jana@gmail.com>
2026-03-04 12:35:44 -0800

    add 06_countdown

---
+++ js/06-countdown.js
@@ -0,0 +1,21 @@
+//Date:04-Mar-2026
+//Author: Jana Schroth
+//Application: Countdown
+
+/*
+Write an application that counts down to 0 based on the number that a user passes into a prompt. 
+If the number is 10, it should count down from 10 to 0. 
+If the number is 100, it should count down from 100 to 0 and so on.
+*/
+
+let startCountDown
+
+//USER PROMPT
+startCountDown = parseInt (prompt ("Enter the number you want to start the count down from:"))
+
+//LOOP
+do {
+    console.log (startCountDown);
+    startCountDown -= 1 ;
+} while (startCountDown >=0);
+
