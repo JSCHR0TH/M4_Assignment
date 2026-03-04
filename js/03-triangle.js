bb75687a84915465c2a12e1aa76b393fca35d23d
Jana Schroth <schroth.jana@gmail.com>
2026-03-04 11:17:51 -0800

    add 03_triangle

---
+++ js/03-triangle.js
@@ -0,0 +1,26 @@
+//Date:04-Mar-2026
+//Author: Jana Schroth
+//Application: Looping a Triangle
+
+/*
+Write a loop that displays the following triangle within a console window:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+let display = "#"
+
+for (i = 1; i <= 7; i++) {
+    if (i===1) {
+        console.log (display)
+    } else {
+        display = display + "#"
+        console.log (display)
+    }
+}
